f3d0071ad915b3b9e579a213b44b87d2e993e8d0
lis76 <rsavoskin@yandex.ru>
2019-05-14 13:56:07 +0300

    Создана ассоциация Юзер и Фото

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+    def landing
+        if user_signed_in?
+          redirect_to "/photos"
+        end
+    end
+
+  def about
+  end
+
+end
+
---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,63 @@
+ class PhotosController < ApplicationController
+    before_action :set_photo, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :owner, only: [:edit, :update, :destroy]
+
+    def index
+      @photos = Photo.all
+    end
+
+    def show
+    end
+
+
+    def new
+      @photo = current_user.photos.build
+    end
+
+
+    def edit
+    end
+
+
+    def create
+      @photo = current_user.photos.build(photo_params)
+
+        if @photo.save
+          redirect_to @photo, notice: 'Photo was successfully created.'
+        else
+          render :new
+        end
+    end
+
+
+    def update
+
+        if @photo.update(photo_params)
+          redirect_to @photo, notice: 'Photo was successfully updated.'
+        else
+          render :edit
+        end
+    end
+
+
+    def destroy
+      @photo.destroy
+        redirect_to photos_url, notice: 'Photo was successfully destroyed.'
+    end
+
+    private
+
+      def owner
+        @photo = current_user.photos.find_by(id: params[:id])
+        redirect_to photos_path, notice: "У Вас нет разрешения на изменения этой фотографии" if @photo.nil?
+      end
+
+      def set_photo
+        @photo = Photo.find(params[:id])
+      end
+
+      def photo_params
+        params.require(:photo).permit(:description)
+      end
+  end
